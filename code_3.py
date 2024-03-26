d6722e091f8530917c1d94219c81b89b6cd1a101
ElenaIT <156838043+LenaX200@users.noreply.github.com>
2024-03-26 01:31:04 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+}
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        }
+    }
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        }
+    public static void main(String[] args) {
+
+
+        int[] data = new int[10];function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+}
+
+
+    data.forEach(item => {
+function main() {
+    }
+main();
+    const data = generateRandomData();
+
+    const data = [];
+}
+    });
